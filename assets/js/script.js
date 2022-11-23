c3d2291f98f34a7ec47c5687e343f3be097bd6ab
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-11-23 17:07:32 +0000

    add exception message

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ async function getStatus(e) {
     if(response.ok) {
         displayStatus(data)
     }else{
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -29,8 +30,23 @@ function displayStatus(data) {
     resultsModal.show()
 }
 
+function processOptions(form) {
+    let optArray = []
+
+    for (let entry of form.entries()) {
+        if (entry[0] === "options") {
+            optArray.push(entry[1]);
+        }
+    }
+    form.delete("options");
+    form.append("options", optArray.join());
+
+    return form;
+
+}
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
                         method: "POST",
@@ -44,6 +60,7 @@ async function postForm(e) {
     if(response.ok) {
         displayErrors(postData)
     }else{
+        displayException(postData);
         throw new Error(postData.error);
     }
 }
@@ -58,7 +75,6 @@ function displayErrors(data) {
         results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`
 
         for (let error of data.error_list) {
-            console.log(error)
             results += `<div>At line <span class="line">${error.line}</span>, `;
             results += `coloumn <span class="column">${error.col}</span></div>`;
             results += `<div class="error">${error.error}</div>`;
@@ -68,5 +84,17 @@ function displayErrors(data) {
         document.getElementById("results-content").innerHTML = results;
         resultsModal.show()
     }
+}
+
+function displayException(data) {
+
+    let issueHeading = `An Exception Occurred`;
+    let issueBody = `<div>The API returned the status code <b>${data.status_code}</b></div>`;
+    issueBody += `<div>Error number: <b>${data.error_no}</b></div>`;
+    issueBody += `<div>Error text: <b>${data.error}</b></div>`
+
+    document.getElementById("resultsModalTitle").innerHTML = issueHeading;
+    document.getElementById("results-content").innerHTML = issueBody;
+    resultsModal.show()
 }
 
